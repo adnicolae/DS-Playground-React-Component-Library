031883c487702c439a24b8a60dea19b377d415cd
Andrei Nicolae <ad.nicolae@outlook.com>
2020-06-21 17:42:30 +0100

    Add react-spring animation

--- src/index.js
+++ src/index.js
@@ -8,44 +8,17 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle, darkTheme, defaultTheme } from './utils';
 import { SignUpModal } from './components/Modals';
 
+
 const App = () => {
   const [ useDarkTheme, setUseDarkTheme ] = useState(false);
+  const [ showModal, setShowModal ] = useState(false);
   return (
     <ThemeProvider theme={ useDarkTheme ? darkTheme : defaultTheme }>
       <GlobalStyle />
-      {/* <button 
-        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }} 
-        onClick={() => setUseDarkTheme(!useDarkTheme)}
-      >
-        Toggle dark theme
-      </button>
-      <div style={{
-        background: useDarkTheme ? defaultTheme.primaryColor : darkTheme.primaryColor,
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-around"
-      }}>
-        <PrimaryButton>Primary Button</PrimaryButton>
-        <br />
-        <SecondaryButton modifiers={[ 'large', 'warning', 'secondaryButtonWarning']}>Secondary Button</SecondaryButton>
-        <br />
-        <SecondaryButton modifiers={[ 'large', 'success', 'secondaryButtonSuccess']}>Secondary Button</SecondaryButton>
-        <br />
-        <SecondaryButton modifiers={[ 'large', 'error', 'secondaryButtonError']}>Secondary Button</SecondaryButton>
-        <br />
-        <TertiaryButton>Tertiary Button</TertiaryButton>
-        <br />
-        <PrimaryButton disabled>Primary Button</PrimaryButton>
-        <br />
-        <SecondaryButton disabled>Secondary Button</SecondaryButton>
-        <br />
-        <TertiaryButton disabled>Tertiary Button</TertiaryButton>
-      </div> */}
-
-      <SignUpModal/>
-
+        <PrimaryButton 
+          style={{ margin: '15px 16px 24px' }}
+          onClick={() => setShowModal(!showModal)}>Toggle modal</PrimaryButton>
+        <SignUpModal showModal={showModal} />
     </ThemeProvider>
   )
 };
