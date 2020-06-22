86c28c5eef3e1f5b97d871980067e2ef7e7a3d47
Andrei Nicolae <ad.nicolae@outlook.com>
2020-06-22 07:58:58 +0100

    Add storybook vodafone theme

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/addons";
+import myTheme from "./vodafoneTheme";
+
+addons.setConfig({
+  theme: vodafoneTheme
+});
---
+++ .storybook/vodafoneTheme.js
@@ -0,0 +1,14 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "light",
+
+  colorSecondary: "#e60000",
+
+  // // Toolbar default and active colors
+  barSelectedColor: "#e60000",
+
+  brandTitle: "Vodafone: UCC",
+  brandUrl: "https://www.vodafone.co.uk/",
+  brandImage: "https://i.imgur.com/Elhfiof.png"
+});
